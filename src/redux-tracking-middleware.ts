6a137551c09411ca2296c1d395eae22f615474e8
Isaque Dias <isaquediasm@gmail.com>
2020-04-17 22:50:57 +0200

    feat(track): add track fn

--- src/redux-tracking-middleware.ts
+++ src/redux-tracking-middleware.ts
@@ -1,6 +1,45 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
+// import "core-js/fn/array.find"
+// ...
+
+const pipe = (...functions: Array<Function>) => (...value: Array<any>) => {
+  return functions.reduce((currentValue, currentFunction) => {
+    return currentFunction(
+      ...(Array.isArray(currentValue) ? currentValue : [currentValue])
+    )
+  }, value)
+}
+
+function track(event: Object) {
+  if (!event) return
+
+  console.log('##event', event)
+}
+
+function validateByTerm(terms: Array<String>) {
+  return (action: any) => {
+    return !terms.some(single => action.type.includes(single)) && action
+  }
+}
+
+function checkValidateMethods() {
+
+}
+
+// should filter out action types containing these strigns
+const filterRules = ['UI', 'LOADING', 'FAILED']
+
 export function trackingMiddleware() {
-  return 'middleware'
+  return (clients: Array<Object>, config: any) => (next: Function) => (
+    action: Object
+  ) => {
+    const { validate } = config
+
+    const validateFn = validate && Array.isArray(validate) ? validate :  
+    const event = pipe(validateByTerm(filterRules))(action)
+
+    track(event)
+
+    return next(action)
+  }
 }
